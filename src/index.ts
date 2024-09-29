c29c8b04af22eed1ce4479b4a0ab6cb2a68abe5c
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-29 01:18:10 +0100

    chore: extract vars from process.env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as github from "@actions/github";
 import * as core from "@actions/core";
 import { Value } from "@sinclair/typebox/value";
-import { envSchema } from "./types/env";
+import { Env, envSchema } from "./types/env";
 import { assistivePricingSettingsSchema, PluginInputs } from "./types/plugin-input";
 import { run } from "./run";
 
@@ -9,7 +9,13 @@ import { run } from "./run";
  * Run the plugin as a GitHub Action instance.
  */
 async function actionRun() {
-  const env = Value.Decode(envSchema, process.env);
+  const payloadEnv = {
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    UBIQUIBOT_PUBLIC_KEY: process.env.UBIQUIBOT_PUBLIC_KEY,
+  };
+
+  const env = Value.Decode(envSchema, payloadEnv);
 
   const webhookPayload = github.context.payload.inputs;
   const settings = Value.Decode(assistivePricingSettingsSchema, Value.Default(assistivePricingSettingsSchema, JSON.parse(webhookPayload.settings)));
