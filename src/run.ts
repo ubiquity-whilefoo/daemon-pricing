72efb74d04b1b5983053e9b999314321f772d8d6
Tomaz Cuk <venooxpersonal@gmail.com>
2024-10-20 22:14:13 +0200

    feat: sync on repo created and issue opened

--- src/run.ts
+++ src/run.ts
@@ -41,6 +41,10 @@ export async function run(inputs: PluginInputs, env: Env) {
 
   const eventName = inputs.eventName;
   switch (eventName) {
+    case "issues.opened":
+    case "repository.created":
+      await syncPriceLabelsToConfig(context);
+      break;
     case "issues.labeled":
     case "issues.unlabeled":
       await syncPriceLabelsToConfig(context);
