cb6b24e8fef4ed8fcbe1bc010bbd6fc4f100c2f5
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-28 22:45:41 +0900

    chore: integrate fetch handler with createPlugin method
    Enhanced plugin setup by adding fetch handler to handle requests

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { createPlugin } from "@ubiquity-os/ubiquity-os-kernel";
+import type { ExecutionContext } from "hono/dist/types/context";
 import { createAdapters } from "./adapters";
 import { run } from "./run";
 import { SupportedEvents } from "./types/context";
@@ -7,17 +8,24 @@ import { Env, envSchema } from "./types/env";
 import { AssistivePricingSettings, pluginSettingsSchema } from "./types/plugin-input";
 import manifest from "../manifest.json";
 
-export default createPlugin<AssistivePricingSettings, Env, SupportedEvents>(
-  (context) => {
-    return run({ ...context, adapters: createAdapters(createClient(context.env.SUPABASE_URL, context.env.SUPABASE_KEY), context) });
+export default {
+  async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
+    return createPlugin<AssistivePricingSettings, Env, SupportedEvents>(
+      (context) => {
+        return run({
+          ...context,
+          adapters: createAdapters(createClient(context.env.SUPABASE_URL, context.env.SUPABASE_KEY), context),
+        });
+      },
+      //@ts-expect-error types are ok
+      manifest,
+      {
+        envSchema: envSchema,
+        postCommentOnError: true,
+        settingsSchema: pluginSettingsSchema,
+        logLevel: env.LOG_LEVEL,
+        kernelPublicKey: env.KERNEL_PUBLIC_KEY,
+      }
+    ).fetch(request, env, executionCtx);
   },
-  //@ts-expect-error types are ok
-  manifest,
-  {
-    envSchema: envSchema,
-    postCommentOnError: true,
-    settingsSchema: pluginSettingsSchema,
-    logLevel: process.env.LOG_LEVEL,
-    kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
-  }
-);
+};
