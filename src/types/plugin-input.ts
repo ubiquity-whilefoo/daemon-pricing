1a37aec446cc49a59044d8ec122dafed65606d04
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-11-27 19:31:25 +0000

    chore: config examples

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -4,21 +4,21 @@ export const pluginSettingsSchema = T.Object(
   {
     globalConfigUpdate: T.Optional(
       T.Object({
-        excludeRepos: T.Array(T.String(), { description: "List of repositories to exclude from being updated" }),
+        excludeRepos: T.Array(T.String(), { examples: ["repo-name", "no-owner-required"], description: "List of repositories to exclude from being updated" }),
       }, { description: "Updates all price labels across all tasks based on `baseRateMultiplier` changes within the config file." }),
     ),
     labels: T.Object(
       {
         time: T.Array(
           T.Object({
-            name: T.String({ description: "The display name of the label representing estimated task length" }),
+            name: T.String({ examples: ["Time: <2 Hours", "Time: <1 Week"], description: "The display name of the label representing estimated task length" }),
             collaboratorOnly: T.Boolean({ default: false, description: "Whether the task is only available for collaborators to be assigned" }),
           }),
           { default: [] }
         ),
         priority: T.Array(
           T.Object({
-            name: T.String({ description: "The display name of the label representing task priority" }),
+            name: T.String({ examples: ["Priority: 1 (Normal)", "Priority: 5 (Emergency)"], description: "The display name of the label representing task priority" }),
             collaboratorOnly: T.Boolean({ default: false, description: "Whether the task is only available for collaborators to be assigned" }),
           }),
           { default: [] }
@@ -26,7 +26,7 @@ export const pluginSettingsSchema = T.Object(
       },
       { default: {} }
     ),
-    basePriceMultiplier: T.Number({ default: 1, description: "The base price multiplier for all tasks" }),
+    basePriceMultiplier: T.Number({ examples: [1.5], default: 1, description: "The base price multiplier for all tasks" }),
     publicAccessControl: T.Object(
       {
         setLabel: T.Boolean({ default: false, description: "Whether to allow anyone to set labels, false to perform permission validation" }),
