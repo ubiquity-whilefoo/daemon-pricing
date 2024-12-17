7591c532a1c1b4de8efac32937c20d1fa1fde2a6
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-17 17:26:36 +0900

    fix: removed `/allow` command

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,3 @@
-import { commandHandlers, handleComment } from "./handlers/comment";
 import { globalLabelUpdate } from "./handlers/global-config-update";
 import { watchLabelChange } from "./handlers/label-change";
 import { onLabelChangeSetPricing } from "./handlers/pricing-label";
@@ -8,15 +7,6 @@ import { isIssueLabelEvent } from "./types/typeguards";
 
 export async function run(context: Context) {
   const { eventName, logger } = context;
-
-  if (context.command) {
-    await commandHandlers[context.command.name](context, {
-      username: context.command.parameters.username,
-      labels: context.command.parameters.labelTypes,
-      command: context.command.name,
-    });
-    return;
-  }
 
   switch (eventName) {
     case "issues.opened":
@@ -32,9 +22,6 @@ export async function run(context: Context) {
       break;
     case "label.edited":
       await watchLabelChange(context);
-      break;
-    case "issue_comment.created":
-      await handleComment(context);
       break;
     case "push":
       await globalLabelUpdate(context);
