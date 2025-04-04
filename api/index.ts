2760032af11a3d3a92c7e947bab04a90fe1df99f
gentlementlegen <fernand.veyrier@epitech.eu>
2025-04-04 19:53:33 +0900

    chore: vercel deploy

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,44 @@
-import { createPlugin } from "@ubiquity-os/plugin-sdk";
-import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
-import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import { handle } from "hono/vercel";
-import manifest from "../manifest.json";
-import { run } from "../src/run";
-import { SupportedEvents } from "../src/types/context";
-import { Env, envSchema } from "../src/types/env";
-import { AssistivePricingSettings, pluginSettingsSchema } from "../src/types/plugin-input";
+// import { createPlugin } from "@ubiquity-os/plugin-sdk";
+// import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
+// import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
+// import { handle } from "hono/vercel";
+// import manifest from "../manifest.json";
+// import { run } from "../src/run";
+// import { SupportedEvents } from "../src/types/context";
+// import { Env, envSchema } from "../src/types/env";
+// import { AssistivePricingSettings, pluginSettingsSchema } from "../src/types/plugin-input";
+//
+// const app = createPlugin<AssistivePricingSettings, Env, null, SupportedEvents>(
+//   async (context) => {
+//     return run(context);
+//   },
+//   manifest as Manifest,
+//   {
+//     envSchema: envSchema,
+//     postCommentOnError: true,
+//     settingsSchema: pluginSettingsSchema,
+//     logLevel: (process.env.LOG_LEVEL as LogLevel) || LOG_LEVEL.INFO,
+//     kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
+//     bypassSignatureVerification: true,
+//     // bypassSignatureVerification: process.env.NODE_ENV === "local",
+//   }
+// );
+//
+// export const GET = handle(app);
+// export const POST = handle(app);
 
-const app = createPlugin<AssistivePricingSettings, Env, null, SupportedEvents>(
-  async (context) => {
-    return run(context);
-  },
-  manifest as Manifest,
-  {
-    envSchema: envSchema,
-    postCommentOnError: true,
-    settingsSchema: pluginSettingsSchema,
-    logLevel: (process.env.LOG_LEVEL as LogLevel) || LOG_LEVEL.INFO,
-    kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
-    bypassSignatureVerification: true,
-    // bypassSignatureVerification: process.env.NODE_ENV === "local",
-  }
-);
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
