e70c5b55ad08dfa1c8b45f9742e2fcd457222938
gentlementlegen <fernand.veyrier@epitech.eu>
2025-04-09 19:11:58 +0900

    chore: api function

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,20 @@
 import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/dist/manifest";
+import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import { handle } from "hono/vercel";
 import manifest from "../manifest.json" with { type: "json" };
+import { envSchema } from "../src/types/env";
+import { pluginSettingsSchema } from "../src/types/plugin-input";
 
-const app = createPlugin(() => {}, manifest as Manifest).basePath("/api");
-
-app.get("/", (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
-});
+const app = createPlugin(() => {}, manifest as Manifest, {
+  envSchema: envSchema,
+  postCommentOnError: true,
+  settingsSchema: pluginSettingsSchema,
+  logLevel: (process.env.LOG_LEVEL as LogLevel) || LOG_LEVEL.INFO,
+  kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
+  bypassSignatureVerification: true,
+  // bypassSignatureVerification: process.env.NODE_ENV === "local",
+}).basePath("/api");
 
 const handler = handle(app);
 
