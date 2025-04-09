4c8e1667f81c47bc2982b7fad4f22719144b7882
gentlementlegen <fernand.veyrier@epitech.eu>
2025-04-09 19:03:06 +0900

    chore: api function

--- api/index.ts
+++ api/index.ts
@@ -11,6 +11,3 @@ const handler = handle(app);
 
 export const GET = handler;
 export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
