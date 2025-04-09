51b10ec7eb69979b54ebe232993421cee81de4e4
gentlementlegen <fernand.veyrier@epitech.eu>
2025-04-09 19:20:45 +0900

    chore: api function

--- src/worker.ts
+++ src/worker.ts
@@ -8,7 +8,7 @@ import type { ExecutionContext } from "hono";
 import manifest from "../manifest.json";
 import { isLocalEnvironment, run } from "./run";
 import { Context, SupportedEvents } from "./types/context";
-import { Env, envSchema } from "../api/types/env";
+import { Env, envSchema } from "../api/env";
 import { AssistivePricingSettings, pluginSettingsSchema } from "./types/plugin-input";
 
 async function startAction(context: Context, inputs: Record<string, unknown>) {
